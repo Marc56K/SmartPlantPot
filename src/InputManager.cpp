78da43a053d42345d20c1bd2c2798d9d9f3d6c4e
Marc Roßbach <>
2021-02-21 19:30:11 +0100

    feat: improve rotary encoder reliability

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,10 +1,9 @@
 #include "InputManager.h"
 
-QueueHandle_t encoderIsrQueue = xQueueCreate(3, sizeof(bool));
+AiEsp32RotaryEncoder rotaryEncoder(ROTENC_A_PIN, ROTENC_B_PIN, -1, -1);
 void IRAM_ATTR EncoderISR()
 {
-    bool val = true;
-    xQueueSend(encoderIsrQueue, &val, 0);
+    rotaryEncoder.readEncoder_ISR();
 }
 
 volatile unsigned long buttonPressedTime = 0;
@@ -19,69 +18,23 @@ void IRAM_ATTR ButtonISR()
     }        
 }
 
-InputManager::InputManager() :
-    _rotaryEncoder(ROTENC_A_PIN, ROTENC_B_PIN, -1, -1),
-    _mutex(xSemaphoreCreateMutex())
+InputManager::InputManager()
 {
 }
 
 InputManager::~InputManager()
 {
-    bool val = false;
-    xQueueSend(encoderIsrQueue, &val, 0);
 }
 
 void InputManager::Init()
 {        
-    _rotaryEncoder.begin();
-
+    rotaryEncoder.begin();
+    rotaryEncoder.setBoundaries(INT16_MIN / 2, INT16_MAX / 2, false);
     attachInterrupt(digitalPinToInterrupt(ROTENC_A_PIN), EncoderISR, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(ROTENC_B_PIN), EncoderISR, CHANGE);
-
-    _rotaryEncoder.setBoundaries(INT16_MIN / 2, INT16_MAX / 2, false);
+    
     pinMode(ROTENC_SW_PIN, INPUT_PULLDOWN);
     attachInterrupt(ROTENC_SW_PIN, ButtonISR, RISING);
-
-    xTaskCreatePinnedToCore(
-      [](void* p) { ((InputManager*)p)->EncoderIsrTaskProc(); },
-      "EncoderISR", /* Name of the task */
-      10000,  /* Stack size in words */
-      this,  /* Task input parameter */
-      1,  /* Priority of the task */
-      &_encoderIsrTask,  /* Task handle. */
-      0); /* Core where the task should run */
-}
-
-void InputManager::EncoderIsrTaskProc()
-{
-    bool run = true;
-    while(run)
-    {
-        xQueueReceive(encoderIsrQueue, &run, portMAX_DELAY);
-        if (run)
-        {
-            _rotaryEncoder.readEncoder_ISR();
-            int16_t encoderPos = _rotaryEncoder.readEncoder();
-            if (_lastEncoderPos != encoderPos)
-            {
-                auto now = millis();
-                unsigned long deltaT = now - _lastEncoderTime;
-                _lastEncoderTime = now;
-                int16_t delta = encoderPos - _lastEncoderPos;
-                _lastEncoderPos = encoderPos;
-                if (deltaT < 50)
-                {
-                    xSemaphoreTake(_mutex, portMAX_DELAY);
-                    {
-                        _encoderDelta += delta;
-                    }
-                    xSemaphoreGive(_mutex); 
-                    //Serial.println(String("RAW ENC: ") + deltaT + " - > " + delta);
-                }
-            }
-        }
-    }
-    vTaskDelete(nullptr);
 }
 
 bool InputManager::ButtonPressed()
@@ -93,17 +46,13 @@ bool InputManager::ButtonPressed()
 
 int InputManager::GetRotaryEncoderDelta()
 {
-    auto result = 0;
-    xSemaphoreTake(_mutex, portMAX_DELAY);
+    const int16_t encoderPos = rotaryEncoder.readEncoder();
+    if (_lastEncoderPos != encoderPos)
     {
-        result = _encoderDelta;
-        _encoderDelta = 0;
+        const int16_t delta = (encoderPos - _lastEncoderPos) / 2;
+        _lastEncoderPos = _lastEncoderPos + 2 * delta;
+        //Serial.println(String("ROT-ENC: ") + delta);
+        return delta;
     }
-    xSemaphoreGive(_mutex); 
-    return result;
-}
-
-unsigned long InputManager::GetLastInputTime()
-{
-    return std::max(_lastEncoderTime, buttonPressedTime);
+    return 0;
 }
--- src/InputManager.h
+++ src/InputManager.h
@@ -11,16 +11,10 @@ public:
     void Init();
     bool ButtonPressed();
     int GetRotaryEncoderDelta();
-    unsigned long GetLastInputTime();
 
 private:
     void EncoderIsrTaskProc();
 
 private:
-    AiEsp32RotaryEncoder _rotaryEncoder;
-    SemaphoreHandle_t _mutex;
-    TaskHandle_t _encoderIsrTask = nullptr;    
-    volatile int16_t _encoderDelta = 0;
-    volatile int16_t _lastEncoderPos = 0;
-    volatile unsigned long _lastEncoderTime = 0;
+    int16_t _lastEncoderPos = 0;
 };
